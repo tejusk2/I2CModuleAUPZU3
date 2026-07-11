cfcb4b959e93d7f6943bfc5968865acd991d13d8
tkrishnan34 <tkrishnan34@gatech.edu>
2026-07-11 19:22:19 -0400

    Created a flanger from comb filter

--- sources_1/new/FilterController.sv
+++ sources_1/new/FilterController.sv
@@ -5,7 +5,14 @@ input logic [15:0] in_data, input logic read_done, input logic rst_n, input logi
 output logic [15:0] out_data, output logic output_ready);
 
     localparam int FILTER_DELAY = 4410; 
-    localparam int ADDR_SIZE = 13; //13 for 4410, 9 for 441
+    localparam int ADDR_SIZE = 11; //13 for 4410, 9 for 441
+
+    localparam int MAXDELAY = 441;
+    localparam int MINDELAY = 41;
+    logic [10:0] flang_counter;
+    logic [6:0] lfo_delay;
+    logic count_up;
+
     logic r_en;
     logic w_en;
     logic [15:0] bram_data_in;
@@ -55,12 +62,11 @@ output logic [15:0] out_data, output logic output_ready);
             read_pointer <= 0;
             r_en <= 0;
             first_read_happened <= 0;
-            read_addr <= 0;
         end else begin
             if(ready)begin
                 r_en <= 1;
                 first_read_happened <= 1;
-                read_addr <= read_pointer;
+                
                 if(read_pointer == FILTER_DELAY-1)begin
                     read_pointer <= 9'd0;
                 end else begin
@@ -69,14 +75,25 @@ output logic [15:0] out_data, output logic output_ready);
             end
         end
     end
+    always_comb begin
+        //Handle flanger read address logic
+        if(write_addr > flang_counter)begin
+            read_addr = write_addr - flang_counter;
+        end else begin
+            read_addr = (MAXDELAY + write_addr) - flang_counter;
+        end
+    end
     //This clock blocked writes the current addition output to memory
     //Sends the data on the negedge of sclk when wclk is low and read_done is high, two cycles have passed
     always_ff @ (negedge sclk)begin
         if(~rst_n)begin
-            write_pointer <= FILTER_DELAY-1;
+            write_pointer <= MAXDELAY-1;
             w_en <= 0;
             output_ready <= 0;
-            write_addr <= FILTER_DELAY-1;
+            write_addr <= MAXDELAY-1;
+            flang_counter <= MAXDELAY-1;
+            count_up <= 0;
+            lfo_delay <= 0;
         end else begin
             //Mostly for the testbench to verify
             if(ready)begin
@@ -90,11 +107,32 @@ output logic [15:0] out_data, output logic output_ready);
                     bram_data_in <= write_data;
                     write_addr <= write_pointer;
                     w_en <= 1;
-                    if(write_pointer == FILTER_DELAY-1)begin
+                    if(write_pointer == MAXDELAY-1)begin
                         write_pointer <= 0;
                     end else begin
                         write_pointer <= write_pointer + 1;
                     end
+                    
+                    if(lfo_delay == 7'd110)begin
+                        lfo_delay <= 0;
+                         //Flanger counter logic
+                        if(count_up)begin
+                            if(flang_counter != MAXDELAY-1)begin
+                                flang_counter <= flang_counter + 1;
+                            end else begin
+                                count_up <= 0;
+                            end
+                        end else begin
+                            if(flang_counter != MINDELAY - 1)begin
+                                flang_counter <= flang_counter - 1;
+                            end else begin
+                                count_up <= 1;
+                            end
+                        end
+                    end else begin
+                        lfo_delay <= lfo_delay + 1;
+                    end
+                   
                 end else begin
                     w_en <= 0;
                 end
